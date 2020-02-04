56130a91de22b3a92436fda169baa7520a1d3c12
1700301020 <1973348815@qq.com>
2020-02-04 12:37:19 +0800

    First commit.

---
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.notificationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String channelId="1";//全局唯一
+    private static final String channelName="First Notification";//渠道名称
+    private static final String channelDescription="First Notification Channel";//设置描述
+    /*importance:重要等级
+    *IMPORTANCE_NONE = 0; 不提示，不展示
+    *IMPORTANCE_MIN = 1; 不提示，在通知下拉栏会展示，但是是收起的
+    *IMPORTANCE_LOW = 2; 会在状态栏中显示，但不会弹窗，通知下拉栏会展示
+    *IMPORTANCE_DEFAULT = 3; 会在状态栏中显示，允许有声音提示，但不会弹窗，通知下拉栏会展示
+    *IMPORTANCE_HIGH = 4; 会弹窗提示，允许有提示音
+    *IMPORTANCE_MAX = 5; 会弹窗提示，允许有提示音，可以使用全屏*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendNotice=findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+
+    public static String createNotificationChannel(Context context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel=new NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription(channelDescription);
+            NotificationManager notificationManager=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+            return channelId;
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.send_notice:
+                NotificationCompat.Builder builder=new NotificationCompat.Builder(getApplicationContext(), Objects.requireNonNull(createNotificationChannel(this)));
+                Notification notification=builder
+                        .setContentTitle("This is content title")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        .build();
+                NotificationManagerCompat.from(getApplicationContext()).notify(1,notification);
+                break;
+            default:
+                break;
+        }
+    }
+}
